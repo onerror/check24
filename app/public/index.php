50e2a609a2abcf2ffc21394c823dfe395db764f4
Evgeny Pyastolov <pyastolov.e.s@effective-group.ru>
2022-05-12 15:41:28 +0300

    some changes

--- app/public/index.php
+++ app/public/index.php
@@ -14,7 +14,7 @@ $log->pushHandler(new Monolog\Handler\StreamHandler('run_app.log', Monolog\Logge
 require __DIR__.'/../src/views/partials/header.php';
 $pdo = new PDO('mysql:dbname=check24;host=mysql', 'pyastolov', '123456', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-$db = DB::getInstance('mysql', 'pyastolov', '123456', 'check24');
+$db = DB::getInstance('mysql', 'pyastolov', '123456', 'test');
 
 $routes = [
     new \DevCoder\Route('login', '/login', [LoginController::class]),
@@ -56,35 +56,6 @@ try {
 } catch (\Exception $exception) {
     header("HTTP/1.0 404 Not Found");
 }
-
-////$user = new User();
-////$user->fetch(1);
-//
-//var_dump($user);
-//die();
-
-
-//$request = $_SERVER['REQUEST_URI'];
-//
-//
-//switch ($request) {
-//    case 'api':
-//    case '':
-//    case '/' :
-//        require __DIR__ . '/../src/views/index.php';
-//        break;
-//    case '/login' :
-//        require __DIR__ . '/../src/views/login.php';
-//        break;
-//    case '/logout' :
-//        require __DIR__ . '/../src/views/logout.php';
-//        break;
-//    default:
-//        http_response_code(404);
-//        require __DIR__ . '/../src/views/404.php';
-//        break;
-//
-//}
 
 require __DIR__ . '/../src/views/partials/footer.php';
 
